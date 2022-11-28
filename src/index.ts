ffb37d580b87e6cfe95be952f6e590c9daaa3250
OpenYong <fkszm3@gmail.com>
2022-11-28 15:12:08 +0900

    Adding in an Interface

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoInput")! as HTMLInputElement;
+// const form = document.querySelector("form")!;
+const form = document.querySelector("#todoForm")! as HTMLFormElement;
+const list = document.getElementById("todoList")!;
 
-btn.addEventListener("click", function () {
-  alert(input.value);
+interface Todo {
+  text: string;
+  isCompleted: boolean;
+}
+
+const todos: Todo[] = [];
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    isCompleted: false,
+  };
+
+  todos.push(newTodo);
+  createTodoElement(newTodo);
+}
+
+function createTodoElement(todo: Todo) {
+  const listItem = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  listItem.append(todo.text);
+  listItem.append(checkbox);
+  list.append(listItem);
   input.value = "";
-});
+}
 
-// let mystery: unknown = "안녕하세요.";
-
-// const numChars = (mystery as string).length;
+form.addEventListener("submit", handleSubmit);
+// btn.addEventListener("click", function () {
+//   alert(input.value);
+//   input.value = "";
+// });
